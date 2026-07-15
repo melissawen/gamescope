c2a1d836622df596171df3e8e2ec8647a0bd82f3
Melissa Wen <mwen@igalia.com>
2026-07-15 07:12:19 -0300

    drm: check if CRTC color pipeline api is supported
    Signed-off-by: Melissa Wen <mwen@igalia.com>

--- src/Backends/DRMBackend.cpp
+++ src/Backends/DRMBackend.cpp
@@ -620,6 +620,7 @@ struct drm_color_ctm2 {
 bool g_bSupportsAsyncFlips = false;
 bool g_bSupportsSyncObjs = false;
 bool g_bSupportsColorPipeline = false;
+bool g_bSupportsCRTCColorPipeline = false;
 
 extern gamescope::GamescopeModeGeneration g_eGamescopeModeGeneration;
 extern GamescopePanelOrientation g_DesiredInternalOrientation;
@@ -1108,6 +1109,16 @@ static bool get_resources(struct drm_t *drm)
 		}
 	}
 
+	if ( g_bSupportsCRTCColorPipeline )
+	{
+		for ( std::unique_ptr< gamescope::CDRMCRTC > &pCRTC : drm->crtcs )
+		{
+			if ( !get_crtc_color_pipelines( drm, pCRTC ) )
+				return false;
+		}
+	}
+
+
 	return refresh_state( drm );
 }
 
@@ -1485,6 +1496,7 @@ bool init_drm(struct drm_t *drm, int width, int height, int refresh)
 	}
 
 	g_bSupportsColorPipeline = drmSetClientCap(drm->fd, DRM_CLIENT_CAP_PLANE_COLOR_PIPELINE, 1) == 0;
+	g_bSupportsCRTCColorPipeline = drmSetClientCap(drm->fd, DRM_CLIENT_CAP_CRTC_COLOR_PIPELINE, 1) == 0;
 
 	if (!get_resources(drm)) {
 		return false;
@@ -3815,6 +3827,9 @@ bool drm_supports_color_mgmt(struct drm_t *drm)
 	if ( g_bSupportsColorPipeline )
 		return false;
 
+	if ( g_bSupportsCRTCColorPipeline )
+		return false;
+
 	if ( !drm->pPrimaryPlane )
 		return false;
 
@@ -3829,10 +3844,13 @@ bool drm_supports_color_pipeline(struct drm_t *drm)
 	if ( !g_bSupportsColorPipeline )
 		return false;
 
+	if ( !g_bSupportsCRTCColorPipeline )
+		return false;
+
 	if ( !drm->pPrimaryPlane )
 		return false;
 
-	return drm->pPrimaryPlane->GetProperties().COLOR_PIPELINE.has_value() ;
+	return drm->pPrimaryPlane->GetProperties().COLOR_PIPELINE.has_value() && drm->pCRTC->GetProperties().COLOR_PIPELINE.has_value();
 }
 
 std::span<const uint32_t> drm_get_valid_refresh_rates( struct drm_t *drm )
